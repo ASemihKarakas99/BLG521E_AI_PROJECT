ee9185b4f487be92048e979139842b82cd250685
Semih Karakas <semih.karakas@btsgrp.com>
2025-05-20 01:29:23 +0300

    Basic agent configurations and environment

--- DQN/dqn.py
+++ DQN/dqn.py
@@ -2,16 +2,15 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-
 class DQN(nn.Module):
-    def __init__(self, state_dim, action_dim, hidden_size=128):
+
+    def __init__(self, state_dim, action_dim, hidden_dim=256):
         super(DQN, self).__init__()
-        self.layer1 = nn.Linear(state_dim, hidden_size)
-        self.layer2 = nn.Linear(hidden_size, action_dim)
-        
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.output = nn.Linear(hidden_dim, action_dim)
+
     def forward(self, x):
-        x = F.relu(self.layer1(x))
-        return self.layer2(x)
-    
-    
-    
+        x = F.relu(self.fc1(x))
+        Q = self.output(x)
+
+        return Q
--- agent.py
+++ agent.py
@@ -53,7 +53,7 @@ class Agent:
 
     def run(self, num_episodes=10000, render=False):
         start_time = datetime.now()
-        last_graph_update_time = start_time
+        # last_graph_update_time = start_time
 
         log_message = f"{start_time.strftime(DATE_FORMAT)}: Training starting..."
         print(log_message)
@@ -74,7 +74,7 @@ class Agent:
         rewards_per_episode = []
 
         # Create policy and target network. Number of nodes in the hidden layer can be adjusted.
-        policy_dqn = DQN(num_states, num_actions, self.fc1_nodes, self.enable_dueling_dqn).to(device)
+        policy_dqn = DQN(num_states, num_actions, self.fc1_nodes).to(device)
 
 
         epsilon = self.epsilon_init
@@ -83,7 +83,7 @@ class Agent:
         memory = ReplayMemory(self.replay_memory_size)
 
         # Create the target network and make it identical to the policy network
-        target_dqn = DQN(num_states, num_actions, self.fc1_nodes, self.enable_dueling_dqn).to(device)
+        target_dqn = DQN(num_states, num_actions, self.fc1_nodes).to(device)
         target_dqn.load_state_dict(policy_dqn.state_dict())
 
         # Policy network optimizer. "Adam" optimizer can be swapped to something else.
@@ -158,10 +158,10 @@ class Agent:
                 best_reward = episode_reward
 
             # Update graph every x seconds
-            current_time = datetime.now()
-            if current_time - last_graph_update_time > timedelta(seconds=10):
-                self.save_graph(rewards_per_episode, epsilon_history)
-                last_graph_update_time = current_time
+            # current_time = datetime.now()
+            # if current_time - last_graph_update_time > timedelta(seconds=10):
+            #     self.save_graph(rewards_per_episode, epsilon_history)
+            #     last_graph_update_time = current_time
 
             # If enough experience has been collected
             if len(memory)>self.mini_batch_size:
@@ -177,3 +177,56 @@ class Agent:
                     target_dqn.load_state_dict(policy_dqn.state_dict())
                     step_count=0
 
+
+    # Optimize policy network
+    def optimize(self, mini_batch, policy_dqn, target_dqn):
+
+        # Transpose the list of experiences and separate each element
+        states, actions, new_states, rewards, terminations = zip(*mini_batch)
+
+        # Stack tensors to create batch tensors
+        # tensor([[1,2,3]])
+        states = torch.stack(states)
+
+        actions = torch.stack(actions)
+
+        new_states = torch.stack(new_states)
+
+        rewards = torch.stack(rewards)
+        terminations = torch.tensor(terminations).float().to(device)
+
+        with torch.no_grad():
+            if self.enable_double_dqn:
+                best_actions_from_policy = policy_dqn(new_states).argmax(dim=1)
+
+                target_q = rewards + (1-terminations) * self.discount_factor_g * \
+                                target_dqn(new_states).gather(dim=1, index=best_actions_from_policy.unsqueeze(dim=1)).squeeze()
+            else:
+                # Calculate target Q values (expected returns)
+                target_q = rewards + (1-terminations) * self.discount_factor_g * target_dqn(new_states).max(dim=1)[0]
+                '''
+                    target_dqn(new_states)  ==> tensor([[1,2,3],[4,5,6]])
+                        .max(dim=1)         ==> torch.return_types.max(values=tensor([3,6]), indices=tensor([3, 0, 0, 1]))
+                            [0]             ==> tensor([3,6])
+                '''
+
+        # Calcuate Q values from current policy
+        current_q = policy_dqn(states).gather(dim=1, index=actions.unsqueeze(dim=1)).squeeze()
+        '''
+            policy_dqn(states)  ==> tensor([[1,2,3],[4,5,6]])
+                actions.unsqueeze(dim=1)
+                .gather(1, actions.unsqueeze(dim=1))  ==>
+                    .squeeze()                    ==>
+        '''
+
+        # Compute loss
+        loss = self.loss_fn(current_q, target_q)
+
+        # Optimize the model (backpropagation)
+        self.optimizer.zero_grad()  # Clear gradients
+        loss.backward()             # Compute gradients
+        self.optimizer.step()       # Update network parameters i.e. weights and biases
+
+if __name__ == "__main__":
+    # agent = Agent("lunar_lander")
+    # agent.run(render=True)  
