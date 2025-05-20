cd99ecce7d7f52894e96b7b5488a5d5b5ef1f372
Semih Karakas <semih.karakas@btsgrp.com>
2025-05-20 14:36:41 +0300

    Prioritized Experience Replay added.

--- buffers/experience_replay.py
+++ buffers/experience_replay.py
@@ -10,22 +10,22 @@ import numpy as np
 
 
 # Simle Implementation of Experience Replay
-class ReplayMemory():
-    def __init__(self, maxlen, seed=None):
-        self.memory = deque([], maxlen=maxlen)
+# class ReplayMemory():
+#     def __init__(self, maxlen, seed=None):
+#         self.memory = deque([], maxlen=maxlen)
 
-        # Optional seed for reproducibility
-        if seed is not None:
-            random.seed(seed)
+#         # Optional seed for reproducibility
+#         if seed is not None:
+#             random.seed(seed)
 
-    def append(self, transition):
-        self.memory.append(transition)
+#     def append(self, transition):
+#         self.memory.append(transition)
 
-    def sample(self, sample_size):
-        return random.sample(self.memory, sample_size)
+#     def sample(self, sample_size):
+#         return random.sample(self.memory, sample_size)
 
-    def __len__(self):
-        return len(self.memory)
+#     def __len__(self):
+#         return len(self.memory)
 
 
 
