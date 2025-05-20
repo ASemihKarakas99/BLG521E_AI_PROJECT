1c76dc26742cef2c522fe482223fb2bc7d2bb536
Semih Karakas <semih.karakas@btsgrp.com>
2025-05-20 14:21:25 +0300

    Vanilla Experience Replay Implementation

--- buffers/experience_replay.py
+++ buffers/experience_replay.py
@@ -1,6 +1,15 @@
 # Define memory for Experience Replay
 from collections import deque
 import random
+
+import torch
+import numpy as np
+
+# from memory.tree import SumTree
+# from memory.utils import device
+
+
+# Simle Implementation of Experience Replay
 class ReplayMemory():
     def __init__(self, maxlen, seed=None):
         self.memory = deque([], maxlen=maxlen)
@@ -17,3 +26,48 @@ class ReplayMemory():
 
     def __len__(self):
         return len(self.memory)
+
+
+
+#  Advanced Implementation of Experience Replay
+class ReplayBuffer:
+    def __init__(self, state_size, action_size, buffer_size):
+        # state, action, reward, next_state, done
+        self.state = torch.empty(buffer_size, state_size, dtype=torch.float)
+        self.action = torch.empty(buffer_size, action_size, dtype=torch.float)
+        self.reward = torch.empty(buffer_size, dtype=torch.float)
+        self.next_state = torch.empty(buffer_size, state_size, dtype=torch.float)
+        self.done = torch.empty(buffer_size, dtype=torch.int)
+
+        self.count = 0
+        self.real_size = 0
+        self.size = buffer_size
+
+    def add(self, transition):
+        state, action, reward, next_state, done = transition
+
+        # store transition in the buffer
+        self.state[self.count] = torch.as_tensor(state)
+        self.action[self.count] = torch.as_tensor(action)
+        self.reward[self.count] = torch.as_tensor(reward)
+        self.next_state[self.count] = torch.as_tensor(next_state)
+        self.done[self.count] = torch.as_tensor(done)
+
+        # update counters
+        self.count = (self.count + 1) % self.size
+        self.real_size = min(self.size, self.real_size + 1)
+
+    def sample(self, batch_size):
+        assert self.real_size >= batch_size
+
+        sample_idxs = np.random.choice(self.real_size, batch_size, replace=False)
+
+        batch = (
+            self.state[sample_idxs].to(device()),
+            self.action[sample_idxs].to(device()),
+            self.reward[sample_idxs].to(device()),
+            self.next_state[sample_idxs].to(device()),
+            self.done[sample_idxs].to(device())
+        )
+        return batch
+    
